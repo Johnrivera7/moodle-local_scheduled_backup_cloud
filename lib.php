b15524e1471a9733007830b1f7687417af34e964
John Rivera Gonzalez <johnriveragonzalez7@gmail.com>
2026-05-06 10:49:49 -0400

    Opción B: configura nube en Copia de seguridad programada (dependiente de checkbox)
    Inyecta ajustes del plugin en section=automated y oculta/mostrar con enabled.
    Deja la página del plugin solo con enlace al informe.
    Versión 2026050601.

--- lib.php
+++ lib.php
@@ -8,7 +8,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Añade al final de «Copia de seguridad programada» (section=automated) un bloque con enlace a los ajustes de subida a la nube.
+ * Extiende «Copia de seguridad programada» (section=automated) con los ajustes de subida a la nube.
  * Los plugins locales se cargan después del núcleo; entonces locate('automated') ya existe.
  *
  * @param admin_root $ADMIN árbol de administración
@@ -30,12 +30,151 @@ function local_scheduled_backup_cloud_extend_scheduled_backup_page(admin_root $A
         get_string('automated_cloud_section_desc', 'local_scheduled_backup_cloud')
     ));
 
+    $dependson = 'local_scheduled_backup_cloud/enabled';
+
+    $enabled = new admin_setting_configcheckbox(
+        $dependson,
+        get_string('enabled', 'local_scheduled_backup_cloud'),
+        get_string('enabled_desc', 'local_scheduled_backup_cloud'),
+        0
+    );
+    $page->add($enabled);
+
+    $provider = new admin_setting_configselect(
+        'local_scheduled_backup_cloud/provider',
+        get_string('provider', 'local_scheduled_backup_cloud'),
+        get_string('provider_desc', 'local_scheduled_backup_cloud'),
+        'google',
+        [
+            'google' => get_string('provider_google', 'local_scheduled_backup_cloud'),
+            'microsoft' => get_string('provider_microsoft', 'local_scheduled_backup_cloud'),
+        ]
+    );
+    $provider->add_dependent_on($dependson);
+    $page->add($provider);
+
+    $oauthheading = new admin_setting_heading(
+        'local_scheduled_backup_cloud/oauth',
+        get_string('oauth_heading', 'local_scheduled_backup_cloud'),
+        get_string('oauth_intro', 'local_scheduled_backup_cloud')
+    );
+    $oauthheading->add_dependent_on($dependson);
+    $page->add($oauthheading);
+
+    $redirect = new moodle_url('/local/scheduled_backup_cloud/oauth_callback.php');
+    $oauthredirect = new admin_setting_description(
+        'local_scheduled_backup_cloud/oauth_redirect_desc',
+        get_string('oauth_redirect', 'local_scheduled_backup_cloud'),
+        html_writer::tag('code', $redirect->out(false))
+    );
+    $oauthredirect->add_dependent_on($dependson);
+    $page->add($oauthredirect);
+
+    $clientid = new admin_setting_configtext(
+        'local_scheduled_backup_cloud/clientid',
+        get_string('clientid', 'local_scheduled_backup_cloud'),
+        '',
+        '',
+        PARAM_RAW
+    );
+    $clientid->add_dependent_on($dependson);
+    $page->add($clientid);
+
+    $clientsecret = new admin_setting_configpasswordunmask(
+        'local_scheduled_backup_cloud/clientsecret',
+        get_string('clientsecret', 'local_scheduled_backup_cloud'),
+        '',
+        ''
+    );
+    $clientsecret->add_dependent_on($dependson);
+    $page->add($clientsecret);
+
+    $refresh = get_config('local_scheduled_backup_cloud', 'oauth_refresh_token');
+    $tokmsg = (is_string($refresh) && $refresh !== '')
+        ? get_string('oauth_connected', 'local_scheduled_backup_cloud')
+        : get_string('oauth_not_connected', 'local_scheduled_backup_cloud');
+    $oauthstatus = new admin_setting_description(
+        'local_scheduled_backup_cloud/oauth_status',
+        '',
+        $tokmsg
+    );
+    $oauthstatus->add_dependent_on($dependson);
+    $page->add($oauthstatus);
+
+    $connecturl = new moodle_url('/local/scheduled_backup_cloud/oauth_start.php');
+    $oauthconnect = new admin_setting_description(
+        'local_scheduled_backup_cloud/oauth_connect_link',
+        get_string('oauth_connect', 'local_scheduled_backup_cloud'),
+        html_writer::link($connecturl, get_string('oauth_connect', 'local_scheduled_backup_cloud'))
+    );
+    $oauthconnect->add_dependent_on($dependson);
+    $page->add($oauthconnect);
+
+    $pathsheading = new admin_setting_heading(
+        'local_scheduled_backup_cloud/paths',
+        get_string('paths_heading', 'local_scheduled_backup_cloud'),
+        get_string('paths_heading_desc', 'local_scheduled_backup_cloud')
+    );
+    $pathsheading->add_dependent_on($dependson);
+    $page->add($pathsheading);
+
+    $sitefolderslug = new admin_setting_configtext(
+        'local_scheduled_backup_cloud/site_folder_slug',
+        get_string('site_folder_slug', 'local_scheduled_backup_cloud'),
+        get_string('site_folder_slug_desc', 'local_scheduled_backup_cloud'),
+        '',
+        PARAM_TEXT
+    );
+    $sitefolderslug->add_dependent_on($dependson);
+    $page->add($sitefolderslug);
+
+    $folderstrategy = new admin_setting_configselect(
+        'local_scheduled_backup_cloud/folder_strategy',
+        get_string('folder_strategy', 'local_scheduled_backup_cloud'),
+        get_string('folder_strategy_desc', 'local_scheduled_backup_cloud'),
+        'site_shortname',
+        [
+            'site_shortname' => get_string('folder_site_shortname', 'local_scheduled_backup_cloud'),
+            'site_idnumber' => get_string('folder_site_idnumber', 'local_scheduled_backup_cloud'),
+        ]
+    );
+    $folderstrategy->add_dependent_on($dependson);
+    $page->add($folderstrategy);
+
+    $filenamepreview = new admin_setting_description(
+        'local_scheduled_backup_cloud/filename_preview',
+        get_string('filename_preview_heading', 'local_scheduled_backup_cloud'),
+        get_string('filename_pattern_fixed_desc', 'local_scheduled_backup_cloud') . html_writer::empty_tag('br') .
+        local_scheduled_backup_cloud_get_filename_preview_html()
+    );
+    $filenamepreview->add_dependent_on($dependson);
+    $page->add($filenamepreview);
+
+    $afterheading = new admin_setting_heading(
+        'local_scheduled_backup_cloud/after_upload',
+        get_string('after_upload_heading', 'local_scheduled_backup_cloud'),
+        get_string('after_upload_desc', 'local_scheduled_backup_cloud')
+    );
+    $afterheading->add_dependent_on($dependson);
+    $page->add($afterheading);
+
+    $deletelocal = new admin_setting_configcheckbox(
+        'local_scheduled_backup_cloud/delete_local_after_upload',
+        get_string('delete_local_after_upload', 'local_scheduled_backup_cloud'),
+        get_string('delete_local_after_upload_desc', 'local_scheduled_backup_cloud'),
+        1
+    );
+    $deletelocal->add_dependent_on($dependson);
+    $page->add($deletelocal);
+
     $settingsurl = new moodle_url('/admin/settings.php', ['section' => 'local_scheduled_backup_cloud']);
-    $page->add(new admin_setting_description(
+    $openplugin = new admin_setting_description(
         'local_scheduled_backup_cloud/automated_hook_link',
         '',
         html_writer::link($settingsurl, get_string('automated_cloud_open_settings', 'local_scheduled_backup_cloud'))
-    ));
+    );
+    $openplugin->add_dependent_on($dependson);
+    $page->add($openplugin);
 }
 
 /**
