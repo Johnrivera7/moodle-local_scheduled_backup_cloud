ec73f4f694e28e484677891b1d62bad98fe538f0
John Rivera Gonzalez <johnriveragonzalez7@gmail.com>
2026-05-06 12:35:59 -0400

    oauth_callback: incluir filelib (clase curl de Moodle).
    config.php no carga filelib; new curl() requiere require explícito.
    
    Versión 2026050607.

--- oauth_callback.php
+++ oauth_callback.php
@@ -6,6 +6,7 @@
  */
 
 require_once(__DIR__ . '/../../config.php');
+require_once($CFG->libdir . '/filelib.php');
 
 require_login();
 require_capability('local/scheduled_backup_cloud:configure', \context_system::instance());
